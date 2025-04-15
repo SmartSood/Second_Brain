533d9b09fe5fcc23eabd67cc60e34ada52b08dc2
SmartSood <133507159+SmartSood@users.noreply.github.com>
2025-04-15 12:44:37 +0530

    first commit skeleton

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+//starting the app and initializing json
+const app=express();
+app.use(express.json());
+
+
+
+app.post("/api/v1/signup",(req,res)=>{
+
+})
+
+app.post("/api/v1/content",(req,res)=>{
+    
+})
+app.post("/api/v1/signup",(req,res)=>{
+    
+})
+app.post("/api/v1/signup",(req,res)=>{
+    
+})
+app.post("/api/v1/signup",(req,res)=>{
+    
+})
+
+app.post("/api/v1/brain/share",(req,res)=>{
+    
+})
+
+
+app.post("/api/v1/brain/:shareLink",(req,res)=>{
+    
+})
+
+
+
+//connecting to the database
